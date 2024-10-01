71770e52368feac7a1f12ca8a7446daaec87300c
0xyzone <58982560+0xyzone@users.noreply.github.com>
2024-10-01 09:37:32 +0545

    push pass

--- app/Filament/App/Resources/TournamentTeamResource/Pages/ListTournamentTeams.php
+++ app/Filament/App/Resources/TournamentTeamResource/Pages/ListTournamentTeams.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\App\Resources\TournamentTeamResource\Pages;
 
+use App\Filament\Imports\TeamPlayerImporter;
 use Filament\Actions;
 use Filament\Actions\ImportAction;
 use Filament\Resources\Pages\ListRecords;
@@ -19,7 +20,11 @@ class ListTournamentTeams extends ListRecords
             // ->slideOver()
             // ->color("primary"),
             ImportAction::make()
+                ->label("Import Teams")
                 ->importer(TournamentTeamImporter::class),
+            ImportAction::make()
+                ->label("Import Players")
+                ->importer(TeamPlayerImporter::class),
             Actions\CreateAction::make()
                 ->label('Add Team'),
         ];
---
+++ app/Filament/Imports/TeamPlayerImporter.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Filament\Imports;
+
+use App\Models\TeamPlayer;
+use Filament\Actions\Imports\ImportColumn;
+use Filament\Actions\Imports\Importer;
+use Filament\Actions\Imports\Models\Import;
+
+class TeamPlayerImporter extends Importer
+{
+    protected static ?string $model = TeamPlayer::class;
+
+    public $tenant = null;
+
+    public function __construct(
+        protected Import $import,
+        protected array $columnMap,
+        protected array $options,
+    ) {
+        $this->tenant = filament()->getTenant();
+    }
+
+    public static function getColumns(): array
+    {
+        return [
+            ImportColumn::make('team')
+                ->relationship(resolveUsing: 'team'),
+            ImportColumn::make('name')
+                ->rules(['max:255']),
+            ImportColumn::make('nickname')
+                ->rules(['max:255']),
+            ImportColumn::make('ingame_id'),
+            ImportColumn::make('gender'),
+        ];
+    }
+
+    public function resolveRecord(): ?TeamPlayer
+    {
+        // return TeamPlayer::firstOrNew([
+        //     // Update existing records, matching them by `$this->data['column_name']`
+        //     'email' => $this->data['email'],
+        // ]);
+
+        return new TeamPlayer([
+            'tournament_id' => $this->tenant->id,
+        ]);
+    }
+
+    public static function getCompletedNotificationBody(Import $import): string
+    {
+        $body = 'Your team player import has completed and ' . number_format($import->successful_rows) . ' ' . str('row')->plural($import->successful_rows) . ' imported.';
+
+        if ($failedRowsCount = $import->getFailedRowsCount()) {
+            $body .= ' ' . number_format($failedRowsCount) . ' ' . str('row')->plural($failedRowsCount) . ' failed to import.';
+        }
+
+        return $body;
+    }
+}
