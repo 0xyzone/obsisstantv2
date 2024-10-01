09b82bc75ff67431c2c8720dd4ac0058c171cf9c
0xyzone <58982560+0xyzone@users.noreply.github.com>
2024-10-01 07:42:09 +0545

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Schedule::call(function () {
+    // Execute the custom queue worker command
+    Artisan::call('queue:custom-worker');
+})->everyMinute();
