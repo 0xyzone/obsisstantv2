1a76219c209068ece3bcfc7a96fda3e748133561
0xyzone <58982560+0xyzone@users.noreply.github.com>
2024-10-06 21:17:08 +0545

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,6 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     server: {
         https: true, // Add this line to enable HTTPS
-        key: "E:/wamp/bin/apache/apache2.4.59/conf/key/obv2.key",
-        cert: "E:/wamp/bin/apache/apache2.4.59/conf/key/obv2.cert"
     },
     plugins: [
         laravel({
