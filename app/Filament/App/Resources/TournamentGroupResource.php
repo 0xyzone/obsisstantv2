8d9cdc2c6a608d77aad20113b56338f231ad5640
0xyzone <58982560+0xyzone@users.noreply.github.com>
2024-10-07 01:48:13 +0545

    Update TournamentGroupResource.php

--- app/Filament/App/Resources/TournamentGroupResource.php
+++ app/Filament/App/Resources/TournamentGroupResource.php
@@ -2,16 +2,17 @@
 
 namespace App\Filament\App\Resources;
 
-use App\Filament\App\Resources\TournamentGroupResource\Pages;
-use App\Filament\App\Resources\TournamentGroupResource\RelationManagers;
-use App\Models\TournamentGroup;
 use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Forms\Form;
 use Filament\Tables\Table;
+use Filament\Facades\Filament;
+use App\Models\TournamentGroup;
+use Filament\Resources\Resource;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\App\Resources\TournamentGroupResource\Pages;
+use App\Filament\App\Resources\TournamentGroupResource\RelationManagers;
 
 class TournamentGroupResource extends Resource
 {
@@ -19,24 +20,21 @@ class TournamentGroupResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-trophy';
     protected static ?string $activeNavigationIcon = 'heroicon-m-trophy';
-
     protected static ?string $tenantOwnershipRelationshipName = 'tournament';
     protected static ?string $tenantRelationshipName = 'groups';
     protected static ?int $navigationSort = 5;
-    
+
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                Forms\Components\Select::make('tournament_id')
-                    ->relationship('tournament', 'name')
-                    ->required(),
                 Forms\Components\TextInput::make('name')
                     ->required()
                     ->maxLength(255),
                 Forms\Components\Toggle::make('is_active')
-                    ->required(),
+                    ->required()
+                    ->inline(false),
             ]);
     }
 
@@ -48,7 +46,7 @@ class TournamentGroupResource extends Resource
                 Tables\Columns\TextColumn::make('name')
                     ->searchable(),
                 Tables\Columns\ImageColumn::make('tournament.logo')
-                ->alignCenter(),
+                    ->alignCenter(),
                 Tables\Columns\IconColumn::make('is_active')
                     ->boolean(),
                 Tables\Columns\TextColumn::make('created_at')
