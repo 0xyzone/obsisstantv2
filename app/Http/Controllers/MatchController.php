9d9273315652f4be743857e77475accdf820f553
0xyzone <58982560+0xyzone@users.noreply.github.com>
2024-11-27 14:54:36 +0545

    update

---
+++ app/Http/Controllers/MatchController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class MatchController extends Controller
+{
+    public function index(User $user) {
+        
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MatchController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::prefix('match')->group(function () {
+    Route::get('/{user}/match', [MatchController::class, 'index']);
+});
