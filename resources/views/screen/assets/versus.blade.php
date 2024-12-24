852a13be48a51c586cd3089b19814e4a0b2c9181
0xyzone <58982560+0xyzone@users.noreply.github.com>
2024-12-24 17:53:29 +0545

    Update versus.blade.php

--- resources/views/screen/assets/versus.blade.php
+++ resources/views/screen/assets/versus.blade.php
@@ -17,7 +17,7 @@
             <div class="w-full h-full flex absolute top-0 justify-center items-center text-2xl font-bold">
                 <div class="flex justify-between w-6/12 items-center">
                     <div class="flex flex-col justify-center items-center">
-                        <div><img src="{{ $activeMatch->teamA->logo ? asset('/storage/' . $activeMatch->teamA->logo) : "" }}" alt="" class="max-w-[20rem]"></div>
+                        <div><img src="{{ $activeMatch->teamA->logo ? asset('/storage/' . $activeMatch->teamA->logo) : "" }}" alt="" class="max-w-[20rem] aspect-square bg-white"></div>
                         <div class="w-max py-2 px-8 bg-gray-200 -skew-x-[30deg] text-6xl font-bold shadow-xl -translate-y-5">
                             <p class="skew-x-[30deg]">{{ $activeMatch->teamA->name }}</p>
                         </div>
@@ -26,7 +26,7 @@
                         <p class="text-9xl">vs</p>
                     </div>
                     <div class="flex flex-col justify-center items-center">
-                        <div><img src="{{ $activeMatch->teamB->logo ? asset('/storage/' . $activeMatch->teamB->logo) : "" }}" alt="" class="max-w-[20rem]"></div>
+                        <div><img src="{{ $activeMatch->teamB->logo ? asset('/storage/' . $activeMatch->teamB->logo) : "" }}" alt="" class="max-w-[20rem] aspect-square bg-white"></div>
                         <div class="w-max py-2 px-8 bg-gray-200 skew-x-[30deg] text-6xl font-bold shadow-xl -translate-y-5">
                             <p class="-skew-x-[30deg]">{{ $activeMatch->teamB->name }}</p>
                         </div>
