d116b1214715dc2f108a80a40183cb493365a9cd
0xyzone <58982560+0xyzone@users.noreply.github.com>
2024-12-25 03:51:55 +0545

    added nav menu

---
+++ app/Filament/App/Pages/TeamBRoster.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Filament\App\Pages;
+
+use Filament\Pages\Page;
+use Filament\Infolists\Infolist;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Infolists\Components\Section;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Infolists\Contracts\HasInfolists;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Infolists\Concerns\InteractsWithInfolists;
+
+class TeamBRoster extends Page implements HasForms, HasInfolists
+{
+    use InteractsWithInfolists;
+    use InteractsWithForms;
+
+    protected static ?string $navigationGroup = 'Assets';
+    protected ?string $subheading = 'Check out the demo of the Team B rooster asset! You can also copy its link to use as a browser source in OBS.';
+    protected static string $view = 'filament.app.pages.team-b-roster';
+    public function infolist(Infolist $infolist): Infolist
+    {
+        return $infolist
+            ->state([
+                'link' => route('screen.teamb', ['id' => auth()->id()]),
+            ])
+            ->schema([
+                Section::make()
+                    ->schema([
+                        TextEntry::make('link')
+                            ->label('OBS Browser Link')
+                            ->copyable()
+                            ->copyMessage('Copied!')
+                            ->copyMessageDuration(1500),
+                    ])
+            ]);
+    }
+}
