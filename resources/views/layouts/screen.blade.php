e01515ff782b09057c9f4ebdc956a34b8cd9a6c5
0xyzone <58982560+0xyzone@users.noreply.github.com>
2024-12-26 11:48:40 +0545

    Update screen.blade.php

--- resources/views/layouts/screen.blade.php
+++ resources/views/layouts/screen.blade.php
@@ -1,6 +1,6 @@
 <head>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<div class="w-[1920px] h-[1080px] relative bg-red-300">
+<div class="w-[1920px] h-[1080px] relative border">
     {{ $slot }}
 </div>
