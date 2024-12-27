6ea627feca02d54bcc90a7fa18d7e25554c9b4ef
0xyzone <58982560+0xyzone@users.noreply.github.com>
2024-12-27 19:53:47 +0545

    Update MatchMakingResource.php

--- app/Filament/App/Resources/MatchMakingResource.php
+++ app/Filament/App/Resources/MatchMakingResource.php
@@ -116,7 +116,7 @@ class MatchMakingResource extends Resource
                         ->relationship(
                             name: 'statsForTeamA',
                             modifyQueryUsing:
-                            fn(Builder $query, Get $get, $record) => $query->where('tournament_team_id', $get('team_a'))->where('match_making_id', $record->id)
+                            fn(Builder $query, Get $get, $record) => $query->where('tournament_team_id', $get('team_a'))->where('match_making_id', ($record ? $record->id : null))
                         )
                         ->addActionLabel('Add player')
                         ->visibleOn('edit')
@@ -259,7 +259,7 @@ class MatchMakingResource extends Resource
                         ->relationship(
                             name: 'statsForTeamB',
                             modifyQueryUsing:
-                            fn(Builder $query, Get $get, $record) => $query->where('tournament_team_id', $get('team_b'))->where('match_making_id', $record->id)
+                            fn(Builder $query, Get $get, $record) => $query->where('tournament_team_id', $get('team_b'))->where('match_making_id', ($record ? $record->id : null))
                         )
                         ->addActionLabel('Add player')
                         ->visibleOn('edit')
