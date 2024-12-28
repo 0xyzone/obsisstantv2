3f60bebf7da231db65df692143d1843c0ef4dbc0
0xyzone <58982560+0xyzone@users.noreply.github.com>
2024-12-28 18:47:16 +0545

    Update versus.blade.php

--- resources/views/screen/assets/versus.blade.php
+++ resources/views/screen/assets/versus.blade.php
@@ -15,19 +15,19 @@
         </div>
         @if ($activeMatch !== null) 
             <div class="w-full h-full flex absolute top-0 items-center text-2xl font-bold">
-                <div class="flex w-max items-center gap-8 mx-auto">
-                    <div class="flex flex-col justify-center items-center">
+                <div class="flex w-full items-center gap-8 mx-auto justify-around">
+                    <div class="flex flex-col justify-center items-center w-4/12">
                         <div><img src="{{ $activeMatch->teamA->logo ? asset('/storage/' . $activeMatch->teamA->logo) : "" }}" alt="" class="min-w-[20rem] max-w-[20rem] aspect-square bg-white object-cover"></div>
-                        <div class="w-4/12 py-2 px-8 bg-gray-200 -skew-x-[30deg] text-6xl font-bold shadow-xl -translate-y-5">
+                        <div class="w-full text-center py-2 px-8 bg-gray-200 -skew-x-[30deg] text-6xl font-bold shadow-xl -translate-y-5">
                             <p class="skew-x-[30deg]">{{ $activeMatch->teamA->name }}</p>
                         </div>
                     </div>
                     <div>
                         <p class="text-9xl">vs</p>
                     </div>
-                    <div class="flex flex-col justify-center items-center">
+                    <div class="flex flex-col justify-center items-center w-4/12">
                         <div><img src="{{ $activeMatch->teamB->logo ? asset('/storage/' . $activeMatch->teamB->logo) : "" }}" alt="" class="min-w-[20rem] max-w-[20rem] aspect-square bg-white object-cover"></div>
-                        <div class="w-4/12 py-2 px-8 bg-gray-200 skew-x-[30deg] text-6xl font-bold shadow-xl -translate-y-5">
+                        <div class="w-full text-center py-2 px-8 bg-gray-200 skew-x-[30deg] text-6xl font-bold shadow-xl -translate-y-5">
                             <p class="-skew-x-[30deg]">{{ $activeMatch->teamB->name }}</p>
                         </div>
                     </div>
