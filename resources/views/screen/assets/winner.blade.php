e1b931fc2e7238f8cea3afca59d4fe1c4bce60e3
0xyzone <58982560+0xyzone@users.noreply.github.com>
2024-12-29 21:18:17 +0545

    Update winner.blade.php

--- resources/views/screen/assets/winner.blade.php
+++ resources/views/screen/assets/winner.blade.php
@@ -2,7 +2,7 @@
     @if ($activeMatch->winner)
     <div class="w-full h-full flex justify-center items-center">
         <div class="flex flex-col items-center w-full">
-            <img src="{{ $activeMatch->winner->logo ? asset('storage/' . $activeMatch->winner->logo) : '' }}" alt="" class="w-64 aspect-square object-cover">
+            <img src="{{ $activeMatch->winner->logo ? asset('storage/' . $activeMatch->winner->logo) : '' }}" alt="" class="w-64 aspect-square object-cover shadow-xl">
             <h1 class="w-6/12 text-4xl px-8 py-2 bg-gray-300 -translate-y-5 text-center">{{ $activeMatch->winner->name }}</h1>
             @if (isset($activeMatch->winner) && $activeMatch->winner->id == $activeMatch->team_a)
             <div class="space-y-2 text-center text-2xl w-6/12 text-white">
