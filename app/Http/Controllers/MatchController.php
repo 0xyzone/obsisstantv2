698ada592fa17e82ecc897af75a082f16ce05ccb
0xyzone <58982560+0xyzone@users.noreply.github.com>
2025-01-05 14:09:37 +0545

    Update MatchController.php

--- app/Http/Controllers/MatchController.php
+++ app/Http/Controllers/MatchController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\MatchMaking;
+use App\Models\Tournament;
 use App\Models\User;
 use Illuminate\Http\Request;
 
 class MatchController extends Controller
 {
     public function index(User $user) {
-        $activeMatch = MatchMaking::where('is_active', true)->where('user_id', $user->id)->with('teamA', 'teamB')->firstOrFail();
+        $activeTournament = Tournament::where('user_id', $user->id)->where('is_active', true)->firstOrFail();
+        $activeMatch = MatchMaking::where('is_active', true)->where('user_id', $user->id)->where('tournament_id', $activeTournament->id)->with('teamA', 'teamB')->firstOrFail();
         return $activeMatch;
     }
 }
