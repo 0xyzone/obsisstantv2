db321a0b91b393dc563ff1e1781483a060719d16
0xyzone <58982560+0xyzone@users.noreply.github.com>
2025-01-05 14:00:29 +0545

    aaiyoraama

--- app/Http/Controllers/MatchController.php
+++ app/Http/Controllers/MatchController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\MatchMaking;
 use App\Models\User;
 use Illuminate\Http\Request;
 
 class MatchController extends Controller
 {
     public function index(User $user) {
-        
+        $activeMatch = MatchMaking::where('is_active', true)->where('user_id', $user->id)->with('teamA', 'teamB')->firstOrFail();
+        return $activeMatch;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,6 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 
-Route::prefix('match')->group(function () {
-    Route::get('/{user}/match', [MatchController::class, 'index']);
+Route::prefix('{user}')->group(function () {
+    Route::get('/match', [MatchController::class, 'index']);
 });
