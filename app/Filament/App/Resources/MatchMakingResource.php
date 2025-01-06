a292496d5eba7eb6d4a326506d9e12eb8af43849
0xyzone <58982560+0xyzone@users.noreply.github.com>
2025-01-06 17:22:13 +0545

    Update MatchMakingResource.php

--- app/Filament/App/Resources/MatchMakingResource.php
+++ app/Filament/App/Resources/MatchMakingResource.php
@@ -83,9 +83,9 @@ class MatchMakingResource extends Resource
                         )
                         ->hidden(fn(Get $get): bool => ($get('team_a') != null && $get('team_b') != null) ? false : true),
                 ])
-                ->key(function ($record){
-                    return $record->id ?? '';
-                })
+                // ->key(function ($record){
+                //     return $record->id ?? '';
+                // })
                     ->headerActions([
                         Action::make('update')
                             ->label('Update')
