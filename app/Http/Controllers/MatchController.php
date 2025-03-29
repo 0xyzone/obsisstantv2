48ea2726f0b2a7f7d834cd65e9b16b81010cdfde
0xyzone <58982560+0xyzone@users.noreply.github.com>
2025-03-29 11:46:12 +0545

    Update MatchController.php

--- app/Http/Controllers/MatchController.php
+++ app/Http/Controllers/MatchController.php
@@ -15,7 +15,7 @@ class MatchController extends Controller
         $tournament = Tournament::query()->whereHas('users', function ($query) use ($userId) {
             $query->where('users.id', $userId);
         })->where('is_active', true)->firstOrFail();
-        $activeMatch = MatchMaking::where('is_active', true)->where('user_id', $user->id)->where('tournament_id', $tournament->id)->with('teamA', 'teamB')->firstOrFail();
+        $activeMatch = MatchMaking::where('is_active', true)->where('user_id', $user->id)->where('tournament_id', $tournament->id)->with('teamA', 'teamB', 'statsForTeamA', 'statsForTeamB')->firstOrFail();
         return $activeMatch;
     }
 
