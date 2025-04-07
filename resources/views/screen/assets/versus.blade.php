3f3e792b3d405b9e2532d2b1332dec4f41191825
0xyzone <58982560+0xyzone@users.noreply.github.com>
2025-04-07 15:38:06 +0545

    Update versus.blade.php

--- resources/views/screen/assets/versus.blade.php
+++ resources/views/screen/assets/versus.blade.php
@@ -17,7 +17,7 @@
             <div class="w-full h-full flex absolute top-0 items-center text-2xl font-bold">
                 <div class="flex w-full items-center gap-8 mx-auto justify-around px-16">
                     <div class="flex flex-col justify-center items-center w-4/12">
-                        <div><img src="{{ $activeMatch->teamA->logo ? asset('/storage/' . $activeMatch->teamA->logo) : "" }}" alt="" class="min-w-[20rem] max-w-[20rem] aspect-square bg-white object-cover"></div>
+                        <div><img src="{{ $activeMatch->teamA->logo ? asset('/storage/' . $activeMatch->teamA->logo) : "" }}" alt="" class="w-64 aspect-square object-scale-down shadow-xl bg-white rounded-lg p-6"></div>
                         <div class="w-full text-center py-2 px-8 bg-gray-200 -skew-x-[30deg] text-6xl font-bold shadow-xl -translate-y-5">
                             <p class="skew-x-[30deg]">{{ $activeMatch->teamA->name }}</p>
                         </div>
@@ -26,7 +26,7 @@
                         <p class="text-9xl text-white">vs</p>
                     </div>
                     <div class="flex flex-col justify-center items-center w-4/12">
-                        <div><img src="{{ $activeMatch->teamB->logo ? asset('/storage/' . $activeMatch->teamB->logo) : "" }}" alt="" class="min-w-[20rem] max-w-[20rem] aspect-square bg-white object-cover"></div>
+                        <div><img src="{{ $activeMatch->teamB->logo ? asset('/storage/' . $activeMatch->teamB->logo) : "" }}" alt="" class="w-64 aspect-square object-scale-down shadow-xl bg-white rounded-lg p-6"></div>
                         <div class="w-full text-center py-2 px-8 bg-gray-200 skew-x-[30deg] text-6xl font-bold shadow-xl -translate-y-5">
                             <p class="-skew-x-[30deg]">{{ $activeMatch->teamB->name }}</p>
                         </div>
