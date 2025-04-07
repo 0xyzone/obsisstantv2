9f42a0e7f4079c23189b42e199399749926c65a1
0xyzone <58982560+0xyzone@users.noreply.github.com>
2025-04-07 15:34:09 +0545

    update

--- resources/views/screen/assets/arosterv2.blade.php
+++ resources/views/screen/assets/arosterv2.blade.php
@@ -3,7 +3,7 @@
     <div class="grid grid-cols-5 items-center max-w-[1720px] gap-2 text-center w-full">
         @foreach ($activeMatch->statsForTeamA as $stat)
         <div class="">
-            <img src="{{ $stat->player->portrait ? asset('storage/' . $stat->player->portrait) : ($stat->player->gender == "male" ? asset('img/placeholder/1.png') : asset('img/placeholder/2.png'))  }}" alt="" class="w-full">
+            <img src="{{ $stat->player->portrait ? asset('storage/' . $stat->player->portrait) : ($stat->player->gender == "male" ? asset('img/placeholder/1w.png') : asset('img/placeholder/2w.png'))  }}" alt="" class="w-full">
 
             <div style="background-color: {{ $tournamentPrimaryColor }}; clip-path: polygon(100% 0%, 98% 19%, 100% 38%, 96% 54%, 100% 61%, 98% 85%, 100% 100%, 0% 100%, 4% 81%, 0 70%, 4% 66%, 0 40%, 3% 28%, 0 20%, 2% 0);
 " class="py-2 {{ $textColor }} w-full drop-shadow-lg">
